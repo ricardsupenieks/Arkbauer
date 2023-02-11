7e9b3c380c709116fe4bf4f2296d5cc6dcd981b4
ricardsupenieks <upenieksrica@gmail.com>
2023-02-11 12:50:08 +0200

    started working on the stock

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Api\V1\CartController;
 use App\Http\Controllers\Api\V1\ProductController;
 use App\Http\Controllers\Api\V1\StockController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,17 +15,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::apiResource('products', ProductController::class);
-
-Route::get('/stock', [StockController::class, 'index']);
-Route::post('/stock', [StockController::class, 'store']);
-Route::delete('/stock/{product}', [StockController::class, 'destroy']);
-Route::put('/stock/{product}', [StockController::class, 'update']);
-Route::get('/stock/{product}', [StockController::class, 'show']);
-
-Route::get('/cart', [CartController::class, 'index']);
-Route::post('/cart', [CartController::class, 'store']);
-Route::delete('/cart/{product}', [CartController::class, 'destroy']);
-Route::put('/cart/{product}', [CartController::class, 'update']);
-Route::get('/cart/{product}', [CartController::class, 'show']);
+Route::apiResource('stock', StockController::class);
+Route::apiResource('cart', CartController::class);
